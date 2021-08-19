83f228c209d6a586d77a6d12af569955a0822454
99005007 <87144281+99005007@users.noreply.github.com>
2021-08-19 07:09:14 +0530

    Add files via upload

---
+++ Activity1/src/ltts/com/Ifelse.java
@@ -0,0 +1,16 @@
+package ltts.com;
+
+public class Ifelse {
+	public static void main(String[] args)
+	{
+		int n=-10;
+		if (n>0) {
+			System.out.println(" number is positive");
+		}
+		else {
+			System.out.println("number is negative");
+		}
+	}
+	
+
+}
